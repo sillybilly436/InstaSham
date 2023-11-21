84c5cfc19ff8bebf09d315c3ad7fe6a2c98f590c
Ben Curtis <BenWCurtis204@gmail.com>
2023-11-20 20:02:46 -0700

    added some front end functionality to uploading images

--- public_html/app/client.js
+++ public_html/app/client.js
@@ -2,3 +2,19 @@
  * Names: Billy Dolny, Ben Curtis, Bronson Housmans
  * Description:
  */
+
+var files = {};
+
+function previewImg() {
+    let uploadImg = document.getElementById('uploadImg');
+    const [file] = uploadImg.files;
+    if (file) {
+        files += file;
+        console.log(file);
+        console.log(files);
+        let current = document.getElementById("previewPhotos");
+        let oldHTML = current.innerHTML;
+        let newHTML = '<img class="createPostImgs" src="' + URL.createObjectURL(file) + '" alt="Your Image"></img>';
+        current.innerHTML = newHTML + oldHTML;
+    }
+}
